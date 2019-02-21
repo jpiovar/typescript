08581d229ee8a74fbe629a5706e53b6bc729e7b6
Juraj Piovar <jpiovar@sk.ibm.com>
2019-02-21 08:14:30 +0100

    protected

--- classes/Man.js
+++ classes/Man.js
@@ -1,3 +1,16 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Man = /** @class */ (function () {
     function Man(fullName) {
         this.name = fullName;
@@ -7,6 +20,20 @@ var Man = /** @class */ (function () {
     };
     return Man;
 }());
+var Superman = /** @class */ (function (_super) {
+    __extends(Superman, _super);
+    function Superman() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.speed = 11;
+        return _this;
+    }
+    Superman.prototype.howFast = function () {
+        console.log(this.name + " has speed " + this.speed);
+    };
+    return Superman;
+}(Man));
 var smallMan = new Man('janko hrasko');
-smallMan.name;
 smallMan.town('hraskovo');
+var superman = new Superman('ferdo mravec');
+superman.speed;
+superman.howFast();
--- classes/Man.ts
+++ classes/Man.ts
@@ -1,14 +1,20 @@
 class Man {
-    public name: string;
-
+    protected name: string;
     public constructor(fullName: string) {
         this.name = fullName;
     }
- 
     public town(townName: string) {
         console.log(`${this.name} is from ${townName}`);
     }
 }
-let smallMan = new Man('janko hrasko');
-smallMan.name;
+class Superman extends Man {
+    readonly speed: number = 11;
+    public howFast() {
+        console.log(`${this.name} has speed ${this.speed}`);
+    }
+}
+var smallMan = new Man('janko hrasko');
 smallMan.town('hraskovo');
+var superman = new Superman('ferdo mravec');
+superman.speed;
+superman.howFast();
